a78550f6f509da95a36f13fc913b77b6b497279b
gizet <lunguucatalin@gmail.com>
2020-01-23 16:38:10 +0200

    [Microservices] Implementing Hysterix and fallback methods.

---
+++ src/main/java/com/sonuswaves/discoveryservice/configuration/SpringConfig.java
@@ -0,0 +1,12 @@
+//package com.sonuswaves.discoveryservice.configuration;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class SpringConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll();
+//    }
+//}
